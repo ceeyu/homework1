99701bdb6ca02f083913c41e60d85348407be2da
ceeyu <shaoxianyou@gmail.com>
2022-05-17 11:17:22 +0800

    hw7quiz7

---
+++ homework/homework7/src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './login';
+// import MultiButton from './cgu_multiButton' //自己的function  class
+// import HelloCgu from './HelloCgu'
+// import CounterButton from './CounterButton'
+//  App.js -- > main
+// called HelloCgu()
+// called mutiButton() 
+// called class function
+function App() {
+  return (
+  
+  <div className="App">
+   {<Header></Header>}
+ 
+  </div>
+  );
+ }
+ export default App;
+
